467141098102dcfdabbf95d84f5bcc603c973c12
parricc <36313429+parricc@users.noreply.github.com>
2026-03-31 18:34:28 -0500

    Add --scaling command-line option

--- app/main.cpp
+++ app/main.cpp
@@ -70,6 +70,7 @@ int main(int argc, char *argv[])
         cout << "  -e <cmd>            Command to execute. This option will catch all following arguments, so use it as the last option." << Qt::endl;
         cout << "  --fullscreen        Run cool-retro-term in fullscreen." << Qt::endl;
         cout << "  -p|--profile <prof> Run cool-retro-term with the given profile." << Qt::endl;
+	cout << "  --scaling <val>     Run cool-retro-term with the given font scaling." << Qt::endl;
         cout << "  -h|--help           Print this help." << Qt::endl;
         cout << "  --verbose           Print additional information such as profiles and settings." << Qt::endl;
         return 0;
@@ -129,6 +130,21 @@ int main(int argc, char *argv[])
 
     engine.rootContext()->setContextProperty("workdir", getNamedArgument(args, "--workdir", QDir::currentPath()));
     engine.rootContext()->setContextProperty("fileIO", &fileIO);
+
+    QString scalingStr = getNamedArgument(args, "--scaling");
+    QVariant cmdScaling;
+
+    if (!scalingStr.isEmpty()) {
+        scalingStr.remove('%'); // Strip the '%' character if present
+        bool ok;
+        float scalingValue = scalingStr.toFloat(&ok);
+        if (ok) {
+            // Convert to a float multiplier (e.g., 60 becomes 0.6)
+            cmdScaling = QVariant(scalingValue / 100.0f);
+        }
+    }
+    // Expose the processed value to the QML frontend
+    engine.rootContext()->setContextProperty("cmdScaling", cmdScaling);
 
     // Manage import paths for Linux and OSX.
     QStringList importPathList = engine.importPathList();
